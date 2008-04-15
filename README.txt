4a49b9018c2c5a2e07a17f94d25000d291010a55
Sylvain Viollon <thefunny@gmail.com>
2008-04-15 10:02:21 +0000

    - Add LICENSE.txt;
    - Fix files headers with copyright information.

--- README.txt
+++ README.txt
@@ -1,4 +1,7 @@
 Introduction
 ============
 
+This extension provide an export feature to ODT for Silva 2.1 and
+higher.
+
 
--- silva/export/opendocument/adapters.py
+++ silva/export/opendocument/adapters.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2008 Infrae. All rights reserved.
+# See also LICENSE.txt
+# $Id$
 
 # Zope 3
 from zope.interface import implements
